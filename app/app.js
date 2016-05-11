40f08bf0812393f64f677cf48045d89eb3877b48
Lucas Rodrigues <Lucas Rodrigues>
2016-05-11 07:39:15 +0100

    Routes

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 var portfolio = anngular.module('portfolio',
                         ['ui.router','ngAnimate','ngRoute']);
                         
-                        /*Routes*/
-                    bring2me.config(routes);
+/*Routes*/
+bring2me.config(routes);
