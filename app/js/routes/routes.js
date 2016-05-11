9a89ec2d710573e79effb7181b0d029cca135e3f
Lucas Rodrigues <Lucas Rodrigues>
2016-05-11 07:21:04 +0100

    routes added

---
+++ app/js/routes/routes.js
@@ -0,0 +1,42 @@
+var routes = function($stateProvider, $urlRouterProvider, $httpProvider){
+    
+    
+    $locationProvider.html5Mode({
+       enabled: true,
+       requireBase: false
+    });
+    
+    $urlRouterProvider.otherwise('/');
+    
+    $stateProvider
+        .state('main',{
+            url:'/',
+            views:{
+                'main':{
+                    templateUrl:'views/main/main.html',
+                    controller:'MainController',
+                    ControllerAs:'main'
+                }
+            }
+        })
+        .state('aboutme',{
+            url:'/about',
+            views:{
+                'about':{
+                    templateUrl:'views/about/about.html',
+                    controller:'AboutController',
+                    ControllerAs:'about'
+                }
+            }
+        })
+        .state('whyme',{
+            url:'/whyme',
+            views:{
+                templateUrl:'views/whyme/whyme.html',
+                controller:'WhyMeController',
+                ControllerAs:'whyme'
+            }
+        });
+}
+
+routes.$inject = ['$stateProvider', '$urlRouterProvider', '$httpProvider'];
