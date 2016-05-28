583ecbb7966267c884d5be73bbee62d924b1fac1
luskr <luskr@DESKTOP-GE0I38R>
2016-05-28 17:19:48 +0100

    added c3JS and cd reference in page

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var portfolio = angular.module('portfolio', ['ui.router', 'ngAnimate', 'ngRoute', 'slickCarousel']);
+var portfolio = angular.module('portfolio', ['ui.router', 'ngAnimate', 'ngRoute', 'slickCarousel','gridshore.c3js.chart']);
 
 /*Controller*/
 portfolio.controller('MainController', MainController);
